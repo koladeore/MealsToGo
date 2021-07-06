897c159bfe6a20b36777b3ef86e7a4718aa521fd
koladeore <koladeore@gmail.com>
2021-07-06 10:15:37 +0100

    update

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import { Searchbar } from "react-native-paper";
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+
+import { SafeArea } from "../../../components/utility/safe-area.components";
+import { RestaurantInfoCard } from "../components/restaurants-info-card.components";
+import { Spacer } from "../../../components/spacer/spacer.components";
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+  console.log(error);
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
